c5e20c73686871ee6113731edb8695dab43483b8
Vikash Sharma <kvikashsharma09@gmail.com>
2023-08-08 22:15:11 +0530

    update code

---
+++ nextjs config/nextjs.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Your other config options go here
+    
+    // Example configuration options:
+    reactStrictMode: true,
+    images: {
+      domains: ['example.com'],
+    },
+    // ...other configurations
+  };
